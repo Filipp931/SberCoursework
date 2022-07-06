ee3865235c84f4b9e9f959f79c1d0940f8b14b15
Fil <fil931@gmail.com>
2022-07-06 11:16:42 +0300

    controller test

--- server/src/test/java/Server/controller/CardControllerTest.java
+++ server/src/test/java/Server/controller/CardControllerTest.java
@@ -1,21 +1,41 @@
 package Server.controller;
 
-import org.hamcrest.Matchers;
+import Server.main.Server;
+import Server.service.CardService;
+import Server.service.CardholderService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.*;
+
 @RunWith(SpringRunner.class)
 @WebMvcTest(CardController.class)
 class CardControllerTest {
     @Autowired
-    private TestRestTemplate template;
+    private MockMvc mockMvc;
+
+    @MockBean
+    CardService cardService;
+    @MockBean
+    CardholderService cardholderService;
+
 
     @Test
     void all() {
